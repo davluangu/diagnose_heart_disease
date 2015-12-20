50f28acb710967d0c599b58e5d0126513d0be339
David Nguyen <david.nguyen@avant.com>
2015-12-19 19:01:05 -0800

    add some how to on exploring the data

--- src/runner.R
+++ src/runner.R
@@ -5,12 +5,25 @@ require(stagerunner)
 options(proj.root = '~/odev/diagnose_heart_disease')
 setwd(options('proj.root')[[1]])
 
+# params for import
+NFOLDERS <- 5
+
 en <- new.env()
 sr <- stagerunner$new(en, list())
 Ramd::define('stages/import', function(import_s) {
   sr$append(stagerunner$new(en, list(
-    'import dicom files' = import_s[[1]](0, 'train')
+    'import dicom files' = import_s[[1]](NFOLDERS, 'data/train')
   )))
 })
 
 sr$run(verbose = TRUE)
+
+## here's how to explore the loaded data:
+# en$dcm_sets %>% length # number of folders loaded
+# en$dcm_sets[[1]]$img %>% length # number of images in the first folder
+# en$dcm_sets[[1]]$img[[1]] %>% dim # image resolution of first image in the first folder
+
+## image resolution of all images in the first folder
+# image_dims <- lapply(en$dcm_sets[[1]]$img, dim) %>% do.call(rbind, .)
+# rownames(image_dims) <- NULL
+# image_dims
