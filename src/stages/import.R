f69eb0dbb66ec6cd22d470caa9533aacf343707c
David Nguyen <david.nguyen@avant.com>
2015-12-19 19:00:36 -0800

    fix paths again

--- src/stages/import.R
+++ src/stages/import.R
@@ -1,14 +1,14 @@
-import_dcm <- function(nsets = 5, path_to_train_data = 'train') {
+import_dcm <- function(nsets = 5, path_to_train_data = 'data/train') {
   require(oro.dicom)
   function(env) {
-    fullpath <- file.path(options('oproj.root')[[1]], path_to_train_data)
+    fullpath <- file.path(options('proj.root')[[1]], path_to_train_data)
     fn   <- list.dirs(fullpath, recursive = TRUE, full.names = TRUE)
     keep <- vapply(fn
       , function(f) length(list.files(f, pattern = 'dcm')) > 0 
       , logical(1))
     fn <- fn[keep]
     if (isTRUE(nsets > 0)) fn <- sample(fn, nsets) 
-    env$dcm_sets <- lapply(fn, readDICOM, verbose = TRUE)
+    env$dcm_sets <- lapply(fn, readDICOM)
   }
 }
 
