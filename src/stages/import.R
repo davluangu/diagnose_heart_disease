64b3a7cf2e9f93425ef88f45fdba93fae6f66739
David Nguyen <david.nguyen@avant.com>
2015-12-19 18:28:54 -0800

    fix param

--- src/stages/import.R
+++ src/stages/import.R
@@ -1,4 +1,4 @@
-import_dcm <- function(nsets = 10, path_to_train_data = 'train') {
+import_dcm <- function(nsets = 5, path_to_train_data = 'train') {
   require(oro.dicom)
   function(env) {
     fullpath <- file.path(options('openproj.root')[[1]], path_to_train_data)
@@ -7,7 +7,7 @@ import_dcm <- function(nsets = 10, path_to_train_data = 'train') {
       , function(f) length(list.files(f, pattern = 'dcm')) > 0 
       , logical(1))
     fn <- fn[keep]
-    if (isTRUE(nsets == 0)) fn <- sample(fn, nsets)
+    if (isTRUE(nsets > 0)) fn <- sample(fn, nsets) 
     env$dcm_sets <- lapply(fn, readDICOM, verbose = TRUE)
   }
 }
